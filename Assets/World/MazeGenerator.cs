4c29cae7d23e5e2ef6db4192e87edc91a6c8006b
IZER <ozee607@gmail.com>
2023-01-29 18:55:07 +0300

    законченный лабиринт

--- Assets/World/MazeGenerator.cs
+++ Assets/World/MazeGenerator.cs
@@ -1,11 +1,11 @@
-using System;
 using System.Collections.Generic;
-using System.Threading;
 
 public class MazeGenerator
 {
     public class MazeCell
     {
+        public int DistanceFromStart;
+
         public int X;
         public int Y;
 
@@ -18,7 +18,7 @@ public class MazeGenerator
     public int Width { get; private set; } = 15;
     public int Height { get; private set; } = 15;
 
-    public MazeCell[,] GenerateMaze()
+    public MazeCell[,] GenerateMaze()  
     {
         var maze = new MazeCell[Width, Height];
 
@@ -26,20 +26,60 @@ public class MazeGenerator
         {
             for (int y = 0; y < maze.GetLength(1); y++)
             {
-                maze[x, y] = new MazeCell() { X = x, Y = y };
+                maze[x, y] = new MazeCell() { X = x, Y = y };   // заполн€ем массив клетками
             }
         }
+
+        for (int x = 0; x < maze.GetLength(0); x++)
+            maze[x, Height - 1].LeftWall = false;
+        for (int y = 0; y < maze.GetLength(0); y++)     // убираем стенки с карЄв
+            maze[Width - 1, y].ButtomWall = false;
+
         RemoveWallBacktracker(maze);
+        PlaceExit(maze);
 
         return maze;
     }
 
+    private void PlaceExit(MazeCell[,] maze)
+    {
+        var furthest = maze[0, 0];
+
+        // находим дальнюю клетку по ’
+        for (int x = 0; x < maze.GetLength(0); x++)
+        {
+            if (maze[x, Height - 2].DistanceFromStart > furthest.DistanceFromStart)
+                furthest = maze[x, Height - 2]; 
+            if (maze[x, 0].DistanceFromStart > furthest.DistanceFromStart)
+                furthest = maze[x, 0];
+        }
+
+        // находим дальнюю клетку по ”
+        for (int y = 0; y < maze.GetLength(1); y++)
+        {
+            if (maze[Width - 2, y].DistanceFromStart > furthest.DistanceFromStart)
+                furthest = maze[Width - 2, y];
+            if (maze[0, y].DistanceFromStart > furthest.DistanceFromStart)
+                furthest = maze[0, y];
+        }
+
+        if (furthest.X == 0)    // если стена на краю лабиринта, то убираем еЄ
+            furthest.LeftWall = false;
+        else if (furthest.Y == 0)
+            furthest.ButtomWall = false;
+        else if (furthest.X == Width - 2)
+            maze[furthest.X + 1, furthest.Y].LeftWall = false;
+        else if (furthest.Y == Height - 2)
+            maze[furthest.X, furthest.Y + 1].ButtomWall = false;
+    }
+
     private void RemoveWallBacktracker(MazeCell[,] maze)
     {
-        MazeCell current = maze[0, 0];
+        MazeCell current = maze[0, 0];  // начальна€ точка лабиринта
         current.Visited = true;
+        current.DistanceFromStart = 0;
 
-        var stack = new Stack<MazeCell>();
+        var stack = new Stack<MazeCell>();  // помещаем в стек клетки которые были выбраны
 
         do
         {
@@ -48,6 +88,7 @@ public class MazeGenerator
             var x = current.X;
             var y = current.Y;
 
+            // добавл€ем непосещЄнную стену в список
             if (x > 0 && !maze[x - 1, y].Visited)
                 notVisited.Add(maze[x - 1, y]);
             if (y > 0 && !maze[x, y - 1].Visited)
@@ -64,12 +105,13 @@ public class MazeGenerator
                 RemoveWall(current, chosen);
 
                 chosen.Visited = true;
+                chosen.DistanceFromStart = stack.Count; // находим дальнюю точку
                 stack.Push(current);
                 current = chosen;
             }
             else
             {
-                current = stack.Pop();
+                current = stack.Pop(); 
             }
 
         } while (stack.Count > 0);
@@ -79,14 +121,14 @@ public class MazeGenerator
     {
         if (current.X == chosen.X)
         {
-            if (current.Y > chosen.Y)
+            if (current.Y > chosen.Y)   // если клетка 1 над клеткой 2, то убираем нижнюю стену
                 current.ButtomWall = false;
             else
                 chosen.ButtomWall = false;
         }
         else
         {
-            if (current.X > chosen.X)
+            if (current.X > chosen.X)   // если клетка 1 правее 2, то убираем левую стену
                 current.LeftWall = false;
             else
                 chosen.LeftWall = false;

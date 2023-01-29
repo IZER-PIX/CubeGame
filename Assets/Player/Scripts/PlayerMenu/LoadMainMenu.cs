90d1573eb39e91501264320dcaac84a8fa2147a0
IZER <ozee607@gmail.com>
2023-01-29 19:01:57 +0300

    finaly

--- Assets/Player/Scripts/PlayerMenu/LoadMainMenu.cs
+++ Assets/Player/Scripts/PlayerMenu/LoadMainMenu.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadMainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Load()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scenes/Menu/GameStart.cs
+++ Assets/Scenes/Menu/GameStart.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameStart : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(1);
     }
 }
